9abd315b4bc7888e9f19e2c9c1c01a9e9d114ded
Jack Kucinski <132936840+JackKuc@users.noreply.github.com>
2025-04-09 13:11:11 -0400

    Create run_encode_atac_ctrl.sh

---
+++ cmds/run_encode_atac_ctrl.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=run_encode_atac
+
+#SBATCH --time=12:00:00
+
+#SBATCH --partition=general
+
+#SBATCH --output=run_encode_atac_ctrl_%j.out
+
+#SBATCH --account=gdkendalllab
+
+#ml ENCODE/caper/2.1.0
+ml ENCODE/caper/2.3.2
+ml Java/18 
+set -x
+INPUT_JSON="ctrl_atac.json"
+caper hpc submit atac.wdl -i "${INPUT_JSON}" --singularity \
+	--leader-job-name ctrl_atac_encode
